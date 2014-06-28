f2e37d96facfa84a511261a223cf2566a82f76bb
Michael Orr <michael@orr.co>
2014-06-28 19:25:40 -0400

    some work for omz and a git clone

--- flywheel/flywheel/omz.py
+++ flywheel/flywheel/omz.py
@@ -1,24 +1,38 @@
-# clone or update OMZ
+import os
 
-ln -s -f ~/.dot/oh-my-zsh ~/.oh-my-zsh
+from git import Repo
+
+from settings import flywheel
+
+settings = flywheel.settings
+
+
+if not os.path.lexists(settings.OMZ.PATH):
+    Repo.clone_from(settings.OMZ.REPO, settings.OMZ.PATH)
+# update the repo
+
+
+for dir_name in settings.OMZ.CUSTOM_DIRS:
+    dir_path = settings.OMZ.PATH + dir_name
+    if not os.path.lexists(dir_path):
+        os.mkdir(dir_path)
+
 
 '''
-if [ ! -d ~/.dot/oh-my-zsh ]; then
-    echo "Cloning OMZ"
-    cd ~/.dot
-    git clone https://github.com/robbyrussel/oh-my-zsh.git
+ln -s -f ~/.dot/oh-my-zsh ~/.oh-my-zsh
+ln -s -f ~/.dot/michaelorr.zsh.theme ~/.dot/oh-my-zsh/custom/themes/michaelorr.zsh-theme
+
+# syntax highlighting
+if [ ! -d ~/.dot/oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    cd ~/.dot/oh-my-zsh/custom/plugins
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 else
     echo "FIXME: update the repo" >&2
 fi
 '''
 
-'''
-# create omz custom folders
-if [ ! -d ~/.dot/oh-my-zsh/custom/plugins ]; then
-    mkdir -p ~/.dot/oh-my-zsh/custom/plugins
-fi
-if [ ! -d ~/.dot/oh-my-zsh/custom/themes ]; then
-    mkdir -p ~/.dot/oh-my-zsh/custom/themes ];
-fi
-'''
-
+for dotfile in settings.OMZ.DOTFILES:
+    source = "%s/%s" % (settings.DOTREPO.PATH, dotfile)
+    destination = "%s/.%s" % (settings.HOME, dotfile)
+    if not os.path.lexists(destination):
+        os.symlink(source, destination)
