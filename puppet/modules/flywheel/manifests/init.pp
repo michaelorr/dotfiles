e7e93e547ac73bd829e046a6bc8265dd10b9ef93
Michael Orr <michael@orr.co>
2014-08-08 11:54:16 -0400

    restructuring

--- puppet/modules/flywheel/manifests/init.pp
+++ puppet/modules/flywheel/manifests/init.pp
@@ -1,32 +1,13 @@
-define flywheel::git_config(
-  $value,
-  $username,
-  $section  = regsubst($name, '^([^\.]+)\.([^\.]+)$','\1'),
-  $key      = regsubst($name, '^([^\.]+)\.([^\.]+)$','\2'),
-) {
-  exec{"git config --global ${section}.${key} '${value}'":
-    #environment => inline_template('<%= "HOME=" + ENV["HOME"] %>'),
-    user        => vagrant,
-    path        => ['/usr/bin', '/bin'],
-    unless      => "git config --global --get ${section}.${key} '${value}'",
-  }
-}
-
-
 class flywheel {
 
-    stage { 'pre':} -> Stage['main'] -> stage {'post':}
+    stage { 'init':} -> stage { 'pre':} -> Stage['main'] -> stage {'post':} -> stage {'final':}
 
     class {'packages':
-        stage => 'pre'
-    }
-
-    class {'git_setup':
-        stage => 'pre'
+        stage => 'init'
     }
 
     class {'clone_repos':
-        stage => 'main'
+        stage => 'pre'
     }
 
     class {'omz_setup':
@@ -37,8 +18,12 @@ class flywheel {
         stage => 'main'
     }
 
-    class {'dotfiles':
+    class {'pindrop_setup':
         stage => 'main'
+    }
+
+    class {'dotfiles':
+        stage => 'final'
     }
 }
 
@@ -54,49 +39,27 @@ class packages {
     package { 'tree': ensure => "latest" }
     package { 'tmux': ensure => "latest" }
     package { 'traceroute': ensure => "latest" }
+    package { 'git': ensure => "latest" }
 
     exec { "apt-get update": command => "/usr/bin/sudo apt-get update"}
 }
 
-class git_setup {
-    include git
-
-
-    exec{"/usr/bin/git config --global user.name 'Michael Orr'":
-        environment => inline_template('<%= "HOME=" + ENV["HOME"] %>'),
-        #user        => vagrant,
-        #path        => ['/usr/bin', '/bin'],
-        #unless      => "git config --global --get ${section}.${key} '${value}'",
-    }
-
-    #flywheel::git_config { 'user.name':
-    #    value => 'Michael Orr',
-    #    username => 'vagrant',
-    #}
-
-    #flywheel::git_config { 'user.email':
-    #    value => 'michael@orr.co',
-    #    username => 'vagrant',
-    #}
-
-    git::config { 'core.excludesfile':
-        value => '~/.dot/gitignore_global',
-    }
-
-    git::config { 'color.ui':
-        value => 'true',
-    }
-}
-
-
 class clone_repos {
+
 }
 
 class omz_setup {
+
 }
 
 class cmg_setup {
+
+}
+
+class pindrop_setup {
+
 }
 
 class dotfiles {
+
 }
