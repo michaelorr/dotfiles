807d1b67ede4a04b2b0242b3b292923e86489c2b
Michael Orr <michael@orr.co>
2014-09-17 11:04:12 -0400

    unbold some things which plays nicer with new font

--- main-highlighter.zsh
+++ main-highlighter.zsh
@@ -34,27 +34,27 @@
 
 # Define default styles.
 : ${ZSH_HIGHLIGHT_STYLES[default]:=none}
-: ${ZSH_HIGHLIGHT_STYLES[unknown-token]:=fg=red,bold}
-: ${ZSH_HIGHLIGHT_STYLES[reserved-word]:=fg=yellow,bold}
-: ${ZSH_HIGHLIGHT_STYLES[alias]:=fg=green,bold}
-: ${ZSH_HIGHLIGHT_STYLES[builtin]:=fg=green,bold}
-: ${ZSH_HIGHLIGHT_STYLES[function]:=fg=green,bold}
-: ${ZSH_HIGHLIGHT_STYLES[command]:=fg=green,bold}
-: ${ZSH_HIGHLIGHT_STYLES[precommand]:=fg=green,underline,bold}
+: ${ZSH_HIGHLIGHT_STYLES[unknown-token]:=fg=red}
+: ${ZSH_HIGHLIGHT_STYLES[reserved-word]:=fg=yellow}
+: ${ZSH_HIGHLIGHT_STYLES[alias]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[builtin]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[function]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[command]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[precommand]:=fg=green,underline}
 : ${ZSH_HIGHLIGHT_STYLES[commandseparator]:=none}
-: ${ZSH_HIGHLIGHT_STYLES[hashed-command]:=fg=green,bold}
+: ${ZSH_HIGHLIGHT_STYLES[hashed-command]:=fg=green}
 : ${ZSH_HIGHLIGHT_STYLES[path]:=underline}
 : ${ZSH_HIGHLIGHT_STYLES[path_prefix]:=underline}
-: ${ZSH_HIGHLIGHT_STYLES[path_approx]:=fg=yellow,underline,bold}
-: ${ZSH_HIGHLIGHT_STYLES[globbing]:=fg=blue,bold}
-: ${ZSH_HIGHLIGHT_STYLES[history-expansion]:=fg=blue,bold}
+: ${ZSH_HIGHLIGHT_STYLES[path_approx]:=fg=yellow,underline}
+: ${ZSH_HIGHLIGHT_STYLES[globbing]:=fg=blue}
+: ${ZSH_HIGHLIGHT_STYLES[history-expansion]:=fg=blue}
 : ${ZSH_HIGHLIGHT_STYLES[single-hyphen-option]:=none}
 : ${ZSH_HIGHLIGHT_STYLES[double-hyphen-option]:=none}
 : ${ZSH_HIGHLIGHT_STYLES[back-quoted-argument]:=none}
-: ${ZSH_HIGHLIGHT_STYLES[single-quoted-argument]:=fg=yellow,bold}
-: ${ZSH_HIGHLIGHT_STYLES[double-quoted-argument]:=fg=yellow,bold}
-: ${ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]:=fg=cyan,bold}
-: ${ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]:=fg=cyan,bold}
+: ${ZSH_HIGHLIGHT_STYLES[single-quoted-argument]:=fg=yellow}
+: ${ZSH_HIGHLIGHT_STYLES[double-quoted-argument]:=fg=yellow}
+: ${ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]:=fg=cyan}
+: ${ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]:=fg=cyan}
 : ${ZSH_HIGHLIGHT_STYLES[assign]:=none}
 
 # Whether the highlighter should be called or not.
