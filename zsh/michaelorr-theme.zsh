2438aebda1a105afe0b42fc7ada4f5de1ef02654
Michael Orr <morr@rsglab.com>
2018-07-24 19:36:32 -0400

    minor visual tweaks to theme

--- zsh/michaelorr-theme.zsh
+++ zsh/michaelorr-theme.zsh
@@ -9,19 +9,19 @@ yellow="%{$fg[yellow]%}"
 purple="%{$fg[purple]%}"
 reset=$'%{\033[0m%}'
 
-ZSH_PROMPT_PREFIX="➜ "
-ZSH_PROMPT_DIVIDER="${orange}❯"
+zsh_prompt_prefix="➜ "
+zsh_prompt_divider="${orange}❯"
 
-ZSH_THEME_GIT_PROMPT_DIRTY="$yellow"
-ZSH_THEME_GIT_PROMPT_CLEAN="$blue"
+export ZSH_THEME_GIT_PROMPT_DIRTY="$yellow$FX[italic]"
+export ZSH_THEME_GIT_PROMPT_CLEAN="$blue"
 
-function zsh_prompt_git() {
+function _zsh_theme::prompt::git() {
         branch=$(git_current_branch)
         [[ -z $branch ]] && return 0
-        echo "$(parse_git_dirty)${branch}${ZSH_PROMPT_DIVIDER}"
+        echo "$(parse_git_dirty)${branch}${reset}${zsh_prompt_divider}"
 }
 
-function git_repo_name() {
+function _zsh_theme::prompt::git::repo() {
     gittopdir=$(git rev-parse --git-dir 2> /dev/null)
 
     if [[ "foo$gittopdir" == "foo.git" ]]; then
@@ -31,41 +31,44 @@ function git_repo_name() {
     fi
 }
 
-function zsh_prompt_dir {
+function _zsh_theme::prompt::dir {
     if git rev-parse --git-dir > /dev/null 2>&1; then
         path_from_git_root=${$(git rev-parse --show-prefix)%/}
 
         if [[ ! -z $path_from_git_root ]]; then
-            dir="$(git_repo_name)/${path_from_git_root}"
+            dir="$(_zsh_theme::prompt::git::repo)/${path_from_git_root}"
         else
-            dir="$(git_repo_name)"
+            dir="$(_zsh_theme::prompt::git::repo)"
         fi
     else
         dir="%~"
     fi
 
-    echo "${blue}${dir}${ZSH_PROMPT_DIVIDER}"
+    echo "${blue}${dir}${zsh_prompt_divider}"
 }
 
-ZSH_PROMPT_RESPONSE_CODE="%(?.$green.$red)$ZSH_PROMPT_PREFIX"
+zsh_prompt_response_code="%(?.$green.$red)$zsh_prompt_prefix"
 
-PS1='${ZSH_PROMPT_RESPONSE_CODE}$(zsh_prompt_dir)$(zsh_prompt_git)${reset} '
+export PS1='${zsh_prompt_response_code}$(_zsh_theme::prompt::dir)$(_zsh_theme::prompt::git)${reset} '
 
 # zsh-users/zsh-syntax-highlighting
 # https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md
 typeset -A ZSH_HIGHLIGHT_STYLES
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
-ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red' 2>/dev/null
-ZSH_HIGHLIGHT_STYLES[comment]='fg=14' 2>/dev/null
+export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
+export ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red' 2>/dev/null
+export ZSH_HIGHLIGHT_STYLES[comment]='fg=14' 2>/dev/null
 
 # zsh-users/zsh-autosuggestions
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=000'
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
 
 # always show colors and file suffixes
-alias ls='ls -GF'
-# See `man ls` on osx for guide to LSCOLORS
-export LSCOLORS=excxdxdxfxdxdxfxfxexex
+export CLICOLOR=1
+alias ls='ls -F'
 # See `man ls` on linux for guide to LS_COLORS
 # Use theme colors when autocompleting files/folders
 export LS_COLORS='no=0:di=34:ln=32:or=32:mi=31:ex=35:st=34:ow=34:tw=34'
 zstyle ':completion:*:default' list-colors "${(s.:.)LS_COLORS}"
+# See `man ls` on osx for guide to LSCOLORS
+# LSCOLORS="excxdxdxfxdxdxfxfxexex"
+export LSCOLORS="excxdxdxfxdxdxfxfxexex"
