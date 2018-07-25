9274b3501407263d9266b77a6f401c3b36d41889
Michael Orr <morr@rsglab.com>
2018-07-24 20:34:32 -0400

    move theme to subfolder to allow splitting out logic

---
+++ zsh/theme/functions.zsh
@@ -0,0 +1,33 @@
+#!/usr/bin/env zsh
+
+function _zsh_theme::prompt::git() {
+        branch=$(git_current_branch)
+        [[ -z $branch ]] && return 0
+        echo "$(parse_git_dirty)${branch}${reset}${zsh_prompt_divider}"
+}
+
+function _zsh_theme::prompt::git::repo() {
+    gittopdir=$(git rev-parse --git-dir 2> /dev/null)
+
+    if [[ "foo$gittopdir" == "foo.git" ]]; then
+        echo $(basename $(pwd))
+    elif [[ "foo$gittopdir" != "foo" ]]; then
+        echo `dirname $gittopdir | xargs basename`
+    fi
+}
+
+function _zsh_theme::prompt::dir {
+    if git rev-parse --git-dir > /dev/null 2>&1; then
+        path_from_git_root=${$(git rev-parse --show-prefix)%/}
+
+        if [[ ! -z $path_from_git_root ]]; then
+            dir="$(_zsh_theme::prompt::git::repo)/${path_from_git_root}"
+        else
+            dir="$(_zsh_theme::prompt::git::repo)"
+        fi
+    else
+        dir="%~"
+    fi
+
+    echo "${blue}${dir}${zsh_prompt_divider}"
+}
---
+++ zsh/theme/michaelorr-theme.zsh
@@ -0,0 +1,52 @@
+setopt prompt_subst
+setopt interactivecomments
+
+source "${0:a:h}/functions.zsh"
+
+local orange="%{$fg[magenta]%}"
+local blue="%{$fg[blue]%}"
+local green="%{$fg[green]%}"
+local red="%{$fg[red]%}"
+local yellow="%{$fg[yellow]%}"
+local purple="%{$fg[purple]%}"
+local reset=$'%{\033[0m%}'
+
+# >>>
+# >>> zsh-users/zsh-syntax-highlighting
+# >>>
+# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md
+export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
+export ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red' 2>/dev/null
+export ZSH_HIGHLIGHT_STYLES[comment]='fg=14' 2>/dev/null
+
+# >>>
+# >>> zsh-users/zsh-autosuggestions
+# >>>
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+
+# >>>
+# >>> LS
+# >>>
+# always show colors and file suffixes
+export CLICOLOR=1
+alias ls='ls -F'
+# See `man ls` on linux for guide to LS_COLORS
+# Use theme colors when autocompleting files/folders
+export LS_COLORS='no=0:di=34:ln=32:or=32:mi=31:ex=35:st=34:ow=34:tw=34'
+zstyle ':completion:*:default' list-colors "${(s.:.)LS_COLORS}"
+# See `man ls` on osx for guide to LSCOLORS
+# LSCOLORS="excxdxdxfxdxdxfxfxexex"
+export LSCOLORS="excxdxdxfxdxdxfxfxexex"
+
+# >>>
+# >>> Prompt
+# >>>
+local zsh_prompt_prefix="➜ "
+local zsh_prompt_divider="${orange}❯"
+
+ZSH_THEME_GIT_PROMPT_DIRTY="$yellow$FX[italic]"
+ZSH_THEME_GIT_PROMPT_CLEAN="$blue"
+zsh_prompt_response_code="%(?.$green.$red)$zsh_prompt_prefix"
+
+PS1='${zsh_prompt_response_code}$(_zsh_theme::prompt::dir)$(_zsh_theme::prompt::git)${reset} '
