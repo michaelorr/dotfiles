a2b74ca08598bb768d919602d0d37c0db5fe6d46
Michael Orr <morr@rsglab.com>
2018-07-30 15:41:24 -0400

    keep zplug until we've removed it entirely

--- zsh/zplug/plugins.zsh
+++ zsh/zplug/plugins.zsh
@@ -4,3 +4,4 @@
 zplug "zsh-users/zsh-autosuggestions"
 zplug 'jsahlen/tmux-vim-integration.plugin.zsh'
 zplug "zsh-users/zsh-syntax-highlighting", defer:2 # defer:2 loads this plugin after compinit
+zplug 'zplug/zplug', hook-build:'zplug --self-manage'
