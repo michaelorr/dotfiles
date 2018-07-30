83a3ff608930c96697beab500c057cad1419578e
Michael Orr <morr@rsglab.com>
2018-07-30 14:10:42 -0400

    restructure a few more things and remove more from zplug plugins

--- zplug/plugins.zsh
+++ zplug/plugins.zsh
@@ -1,8 +1,5 @@
 #!/usr/bin/env zsh
 
-### Local
-zplug "$DOT/zplug/vpn", from:local, as:command
-
 ### OMZ
 zplug "plugins/tmux", from:oh-my-zsh
 
@@ -10,6 +7,3 @@ zplug "plugins/tmux", from:oh-my-zsh
 zplug "zsh-users/zsh-autosuggestions"
 zplug 'jsahlen/tmux-vim-integration.plugin.zsh'
 zplug "zsh-users/zsh-syntax-highlighting", defer:2 # defer:2 loads this plugin after compinit
-
-### Self
-zplug 'zplug/zplug', hook-build:'zplug --self-manage'
