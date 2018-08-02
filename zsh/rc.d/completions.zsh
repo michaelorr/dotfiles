121c5e4acb8945ca971f475290498b392baac949
Michael Orr <morr@rsglab.com>
2018-08-01 22:28:40 -0400

    show menu on first <tab>

--- zsh/rc.d/completions.zsh
+++ zsh/rc.d/completions.zsh
@@ -43,6 +43,7 @@ setopt complete_aliases          # complete alisases
 setopt always_to_end            # when completing from the middle of a word, move the cursor to the end of the word    
 setopt list_ambiguous           # complete as much of a completion until it gets ambiguous.
 setopt complete_in_word         # allow completion from within a word/phrase
+setopt menu_complete
 
 
 
