527ae1d52e78c573f273c7545060b6fb2264199d
Michael Orr <morr@rsglab.com>
2019-08-11 14:08:07 -0400

    Remove old logic for async prompt

--- zsh/theme/functions.zsh
+++ zsh/theme/functions.zsh
@@ -23,29 +23,6 @@ function _zsh_theme::prompt::dir {
 
 function _zsh_theme::async::git_status() {
     gitstatus_query -d $PWD -c _zsh_theme::async::gitstatus_callback -t 0 GSD
-    # typeset -g _ZSH_ASYNC_GIT_DIRTY_FD
-
-    # reset the format before cmd is drawn
-    # export ZSH_THEME_GIT_PROMPT_FORMAT="$ZSH_THEME_GIT_PROMPT_CLEAN"
-
-    # close existing file descriptor if it exists
-    # { exec {_ZSH_ASYNC_GIT_DIRTY_FD}<&- || true } 2>/dev/null
-
-    # if we are in a git repo, run slow cmd via process substitution attached to our fd
-    # if [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) = "true" ]]; then
-    #     exec {_ZSH_ASYNC_GIT_DIRTY_FD}< <(
-    #          if [[ "$(command git config --get zsh-morr.large-repo)" ]]; then
-    #              # In repos marked large, don't spool up threads instantly.
-    #              # This eases some burdon on the underlying hardware.
-    #              # TODO find a way to make this a singleton process
-    #              sleep 0.15
-    #          fi
-    #          exec git status
-    #     )
-    # fi
-
-    # Set callback to our fd
-    # zle -F "$_ZSH_ASYNC_GIT_DIRTY_FD" _zsh_theme::async::git_status_callback
 }
 
 function _zsh_theme::async::gitstatus_callback() {
@@ -68,15 +45,5 @@ function _zsh_theme::branch_styles() {
     local branch_style="ZSH_THEME_GIT_PROMPT_$1"
     export ZSH_THEME_GIT_PROMPT_FORMAT="${(P)branch_style}"
 }
-
-# function _zsh_theme::async::git_status_callback() {
-#     # FD is only argument to this handler
-#     if [[ -n "$(<&$1)" ]]; then
-#         export ZSH_THEME_GIT_PROMPT_FORMAT="$ZSH_THEME_GIT_PROMPT_DIRTY"
-#         zle reset-prompt && zle -R # redraw prompt
-#     fi
-#     zle -F "$1" # remove callback handler from fd
-#     exec {1}<&- # close the fd
-# }
 
 precmd_functions+=_zsh_theme::async::git_status
