6630b400e537e1f9036c295e6172ea8fa3000acb
Michael Orr <michael@orr.co>
2022-04-07 11:52:57 -0400

    add m1 paths to zsh-syntax-highlighting loader

--- zsh/rc.d/zsh-syntax-highlighting.zsh
+++ zsh/rc.d/zsh-syntax-highlighting.zsh
@@ -4,6 +4,8 @@ if [ -f "/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]
     source "/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 elif [ -f "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
     source "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+elif [ -f "/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
+    source "/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 else
     echo "Install zsh-syntax-highlighting [mo]"
 fi
