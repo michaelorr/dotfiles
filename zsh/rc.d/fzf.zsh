bcf00d09af9a241ad139fd2b75ef6fed1898d00a
Michael Orr <michael@orr.co>
2022-04-07 11:52:00 -0400

    remove redundant check and add m1 paths to fzf key binding loader

--- zsh/rc.d/fzf.zsh
+++ zsh/rc.d/fzf.zsh
@@ -1,11 +1,12 @@
-#!/usr/bin/env zsh
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && [[ -f "/usr/local/opt/fzf/shell/completion.zsh" ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-[[ $- == *i* ]] && [[ -f "/usr/share/zsh/vendor-completions/_fzf" ]] && source "/usr/share/zsh/vendor-completions/_fzf" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/share/zsh/vendor-completions/_fzf" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
 [[ -f "/usr/local/opt/fzf/shell/key-bindings.zsh" ]] && source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 [[ -f "/usr/share/doc/fzf/examples/key-bindings.zsh" ]] && source "/usr/share/doc/fzf/examples/key-bindings.zsh"
+[[ -f "/opt/homebrew/opt/fzf/shell/key-bindings.zsh" ]] && source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
