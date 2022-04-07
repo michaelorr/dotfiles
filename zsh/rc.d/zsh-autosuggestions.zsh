b27ba8d01a9262fdee16f56395aaa7a7ac1a2d44
Michael Orr <michael@orr.co>
2022-04-07 11:52:31 -0400

    add m1 paths to zsh-autosuggestions loader

--- zsh/rc.d/zsh-autosuggestions.zsh
+++ zsh/rc.d/zsh-autosuggestions.zsh
@@ -4,6 +4,8 @@ if [ -f "/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
     source "/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 elif [ -f "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
     source "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+elif [ -f  "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
+    source "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 else
     echo "Install zsh-autosuggestions [mo]"
 fi
