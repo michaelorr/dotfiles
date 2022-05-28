931cc54bba2d6691384da9a68517388b113a13d8
Michael Orr <michael@orr.co>
2022-05-28 10:46:44 -0400

    first pass on new mbp

--- zsh/rc.d/zsh-autosuggestions.zsh
+++ zsh/rc.d/zsh-autosuggestions.zsh
@@ -4,7 +4,7 @@ if [ -f "/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
     source "/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 elif [ -f "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
     source "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-elif [ -f  "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
+elif [ -f "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
     source "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 else
     echo "Install zsh-autosuggestions [mo]"
