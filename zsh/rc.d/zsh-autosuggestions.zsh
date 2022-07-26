c9e2293ed40bde556ca1ee85a6b583a44b15d64a
Michael Orr <michael@orr.co>
2022-07-26 15:53:52 -0400

    simplify zsh-syntax-highlighting and zsh-autosuggestions sourcing

--- zsh/rc.d/zsh-autosuggestions.zsh
+++ zsh/rc.d/zsh-autosuggestions.zsh
@@ -1,11 +1,3 @@
 #! /usr/bin/env zsh
 
-if [ -f "/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
-    source "/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-elif [ -f "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
-    source "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-elif [ -f "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
-    source "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-else
-    echo "Install zsh-autosuggestions [mo]"
-fi
+source "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
--- zsh/rc.d/zsh-syntax-highlighting.zsh
+++ zsh/rc.d/zsh-syntax-highlighting.zsh
@@ -1,11 +1,3 @@
 #! /usr/bin/env zsh
 
-if [ -f "/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
-    source "/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-elif [ -f "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
-    source "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-elif [ -f "/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
-    source "/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-else
-    echo "Install zsh-syntax-highlighting [mo]"
-fi
+source "/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
