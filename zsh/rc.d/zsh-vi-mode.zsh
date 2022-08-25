1394eb37078a14574f142c8f005b48939f6f726b
Michael Orr <michael@orr.co>
2022-08-25 17:06:17 -0400

    fix cursor in vim and tweak sourcing of zsh-vi-mode

--- zsh/rc.d/zsh-vi-mode.zsh
+++ zsh/rc.d/zsh-vi-mode.zsh
@@ -1,2 +1,2 @@
 ZVM_INIT_MODE=sourcing
-[[ -d $DOT/zsh/zsh-vi-mode ]] && source $DOT/zsh/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+source $DOT/zsh/zsh-vi-mode/zsh-vi-mode.plugin.zsh || echo "failed to source zsh-vi-mode"
