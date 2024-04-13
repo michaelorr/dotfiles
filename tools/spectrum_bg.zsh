821bdaeaf57a371c92078747e83ab899ce1c29fb
Michael Orr <michael@orr.co>
2024-04-13 11:08:28 -0400

    updates to color map tools

--- tools/spectrum_bg.zsh
+++ tools/spectrum_bg.zsh
@@ -1,9 +1,21 @@
 #!/usr/bin/env zsh
+
+# usage:
+# ./spectrum_bg.zsh
+# fg=122 ./spectrum_bg.zsh
+# g=14 b=72 ./spectrum_bg.zsh
+# r=12 g=14 b=72 ./spectrum_bg.zsh
+
 ZSH_SPECTRUM_TEXT=${ZSH_SPECTRUM_TEXT:-Arma virumque cano Troiae qui primus ab oris}
+FG_RESET="\x1b[39" # RESET TO DEFAULT
+[[ ! -z $fg ]] && FG_OVERRIDE='\x1b[38;5;${fg}m'            # FG="\x1b[38;5;NNNm"         # color0-color255 foreground
+if [[ ! -z $r || ! -z $b || ! -z $g ]]; then
+    r=${r:-0}
+    b=${b:-0}
+    g=${g:-0}
+    FG_OVERRIDE='\x1b[38;2;${r};${g};${b}m'   # FG="\x1b[38;2;RRR;GGG;BBBm" # RGB foreground
+fi
 
 for code in {000..255}; do
-    # FG="\x1b[38;2;RRR;GGG;BBBm" # RGB background
-    # FG="\x1b[38;5;NNNm"         # color0-color255 background
-    FG="\x1b[39" # default foreground
-    print -P -- "$code: $FG %{$BG[$code]%}$ZSH_SPECTRUM_TEXT%{$FX[reset]%}"
+    print -P -- "$code %{$FG_RESET$FG_OVERRIDE$BG[$code]%}$ZSH_SPECTRUM_TEXT%{$FX[reset]%}"
 done
--- tools/spectrum_fg.zsh
+++ tools/spectrum_fg.zsh
@@ -1,9 +1,25 @@
 #!/usr/bin/env zsh
+
+# usage:
+# ./spectrum_fg.zsh
+# bg=122 ./spectrum_fg.zsh
+# r=12 b=72 ./spectrum_fg.zsh
+# r=12 b=72 g=12 ./spectrum_fg.zsh
+
 ZSH_SPECTRUM_TEXT=${ZSH_SPECTRUM_TEXT:-Arma virumque cano Troiae qui primus ab oris}
+BG_RESET="\x1b[49" # RESET TO DEFAULT
+
+# BG="\x1b[48;5;NNNm"         # color0-color255 background
+[[ ! -z $bg ]] && BG_OVERRIDE='\x1b[48;5;${bg}m'
+
+# BG="\x1b[48;2;RRR;GGG;BBBm" # RGB background
+if [[ ! -z $r || ! -z $b || ! -z $g ]]; then
+    r=${r:-0}
+    b=${b:-0}
+    g=${g:-0}
+    BG_OVERRIDE='\x1b[48;2;${r};${g};${b}m'
+fi
 
 for code in {000..255}; do
-    # BG="\x1b[48;2;RRR;GGG;BBBm" # RGB foreground
-    # BG="\x1b[48;5;NNNm"         # color0-color255 forground
-    BG="\x1b[49" # default background
-    print -P -- "$code: $BG %{$FG[$code]$FX[bold]%}$ZSH_SPECTRUM_TEXT%{$FX[reset]%}"
+    print -P -- "$code: %{$BG_RESET$BG_OVERRIDE$FG[$code]$FX[bold]%}$ZSH_SPECTRUM_TEXT%{$FX[reset]%}"
 done
