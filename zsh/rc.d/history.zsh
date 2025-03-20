ebf35633035c216885424d798bd0ef6275b23510
Michael Orr <michael@orr.co>
2025-03-20 15:55:51 -0400

    format zsh configs

--- zsh/rc.d/history.zsh
+++ zsh/rc.d/history.zsh
@@ -10,19 +10,21 @@
 # !?foo         # The last command that matches the pattern `foo'.
 # !?foo?1       # The second word of the last command line that matches `foo'.
 
-setopt append_history # Don't overwrite, append!
-setopt hist_expire_dups_first # Expire duplicate entries first when trimming history.
-setopt hist_fcntl_lock # use OS file locking
-setopt hist_ignore_all_dups # Delete old recorded entry if new entry is a duplicate.
-setopt hist_ignore_space # Don't record an entry starting with a space.
-setopt hist_lex_words # better word splitting, but more CPU heavy
-setopt hist_no_store # Don't store `history` in the history file
-setopt hist_reduce_blanks # Remove superfluous blanks before recording entry.
-setopt hist_save_no_dups # Don't write duplicate entries in the history file.
-setopt hist_verify # Perform history expansion without directly executing.
-setopt inc_append_history # Write after each command
-setopt share_history # share history between multiple shells
+setopt append_history          # Don't overwrite, append!
+setopt hist_expire_dups_first  # Expire duplicate entries first when trimming history.
+setopt hist_fcntl_lock         # use OS file locking
+setopt hist_ignore_dups        # Don't record an entry that was just recorded again.
+setopt hist_ignore_all_dups    # Delete old recorded entry if new entry is a duplicate.
+setopt hist_ignore_space       # Don't record an entry starting with a space.
+setopt hist_lex_words          # better word splitting, but more CPU heavy
+setopt hist_no_store           # Don't store `history` in the history file
+setopt hist_reduce_blanks      # Remove superfluous blanks before recording entry.
+setopt hist_save_no_dups       # Don't write duplicate entries in the history file.
+setopt hist_verify             # Perform history expansion without directly executing.
+setopt inc_append_history      # Write after each command
+setopt share_history           # share history between multiple shells
+setopt extended_history        # Write the history file in the ":start:elapsed;command" format.
 
 [ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
-HISTSIZE=10000000
-SAVEHIST=10000000
+export HISTSIZE=10000000
+export SAVEHIST=10000000
