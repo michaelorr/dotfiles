c894091fdae5dd3fe1d4b9831a3f65a5fcb07bc0
Michael Orr <michael@orr.co>
2025-03-23 16:42:26 -0400

    Add commenting plugin

---
+++ nvim/lua/plugins/comments.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'terrortylor/nvim-comment',
+    event = "VeryLazy",
+    config = function()
+      require("nvim_comment").setup({
+        comment_empty = false,
+      })
+    end
+  }
+}
