02c61ca7c0c49d59c8f2e97a0db34f3088242bd6
Michael Orr <michael@orr.co>
2025-03-23 16:43:39 -0400

    hide some commands from which-key

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,11 +1,37 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    preset = "modern",
+    plugins = {
+      marks = false,
+      spelling = {
+        suggestions = 10,
+      },
+    },
+    spec = {
+      {
+        hidden = true,
+        mode = { "n" },
+        { "za", desc = "Toggle fold under cursor" },
+        { "zA", desc = "Toggle all folds under cursor" },
+        { "zb", desc = "Bottom this line" },
+        { "zd", desc = "Delete fold under cursor" },
+        { "zD", desc = "Delete all folds under cursor" },
+        { "ze", desc = "Right this line" },
+        { "zE", desc = "Delete all folds in file" },
+        { "zf", desc = "Create fold" },
+        { "zH", desc = "Half screen to the left" },
+        { "zs", desc = "Left this line" },
+        { "zL", desc = "Half screen to the right" },
+        { "zt", desc = "Top this line" },
+        { "zv", desc = "Show cursor line" },
+        { "z<CR>", desc = "Top this line" },
+      },
+    },
   },
+
   keys = {
     {
       "<leader>?",
