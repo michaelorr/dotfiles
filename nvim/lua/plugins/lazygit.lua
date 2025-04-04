8512b8db14e331b79b25ab719dc2efdf4a4af6e8
Michael Orr <michael@orr.co>
2025-04-04 16:19:09 -0400

    configure lazygit

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -1,10 +1,17 @@
+-- Normal mode:
+-- <leader>gg - LazyGit
+-- <leader>lg - LazyGit
+
 return {
   "kdheepak/lazygit.nvim",
   lazy = true,
   cmd = { "LazyGit" },
   dependencies = { "nvim-lua/plenary.nvim" },
-  keys = { { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" } },
+  keys = {
+    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  },
   config = function()
-    vim.g.lazygit_floating_window_use_plenary = 0
+    vim.g.lazygit_floating_window_use_plenary = 1
   end,
 }
