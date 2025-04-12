dbca2ee2f50605e1c3a06ef38ca78718ef6c10a2
Michael Orr <michael@orr.co>
2025-04-12 16:48:48 -0400

    replace project plugin

--- nvim/lua/plugins/project.lua
+++ nvim/lua/plugins/project.lua
@@ -1,8 +1,22 @@
 return {
-  "ahmedkhalf/project.nvim",
+  "LintaoAmons/cd-project.nvim",
   config = function()
-    require("project_nvim").setup {
+    require("cd-project").setup {
       patterns = { "!=gruvbox", "!=gitstatus", "!=zsh-vi-mode", ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+
+      projects_config_filepath = vim.fs.normalize(vim.fn.stdpath("config") .. "/cd-project.nvim.json"),
+      project_dir_pattern = { "!=gruvbox", "!=gitstatus", "!=zsh-vi-mode", ".git", ".gitignore", "Cargo.toml", "package.json", "go.mod" },
+      choice_format = "both",        -- optional, you can switch to "name" or "path"
+      projects_picker = "telescope", -- optional, you can switch to `telescope`
+      auto_register_project = false, -- optional, toggle on/off the auto add project behaviour
+      hooks = {
+        {
+          trigger_point = "AFTER_CD",
+          callback = function(_)
+            vim.cmd("Telescope git_files")
+          end
+        },
+      }
     }
   end
 }
