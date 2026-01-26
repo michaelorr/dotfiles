d8cde9e74a7fabae52e24ad9466e8826c304c82c
Michael Orr <michael@orr.co>
2026-01-25 23:13:48 -0500

    Swap zsh-syntax-highlighting for fast-syntax-highlighting
    The former is unmaintained for several years now

---
+++ zsh/rc.d/fast-syntax-highlighting.zsh
@@ -0,0 +1,3 @@
+#! /usr/bin/env zsh
+
+zsh-defer source "${DOT}/zsh/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
--- zsh/rc.d/zsh-syntax-highlighting.zsh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env zsh
-
-zsh-defer source "/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
