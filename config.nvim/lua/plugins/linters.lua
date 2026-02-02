d475ad1140b3dd2578054a7d001d9c232730f1b5
Michael Orr <michael@orr.co>
2026-02-02 15:44:40 -0500

    enable actionlint in nvim

--- config.nvim/lua/plugins/linters.lua
+++ config.nvim/lua/plugins/linters.lua
@@ -2,6 +2,9 @@ return {
   {
     "mfussenegger/nvim-lint",
     opts = {
+      linters_by_ft = {
+        yaml = { "actionlint" },
+      },
       linters = {
         ["markdownlint-cli2"] = {
           args = { "--config", vim.fn.expand("~/.markdownlint.yaml"), "--" },
