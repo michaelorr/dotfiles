a177de53d63d033264908b7db8d7fbf8b4c52364
Michael Orr <michael.orr@cfacorp.com>
2026-06-18 11:40:01 -0400

    Add java lsp/lazyextra, remove copilot plugin, patch LSP registration

--- config.nvim/init.lua
+++ config.nvim/init.lua
@@ -1,2 +1,15 @@
+do
+  local handler = vim.lsp.handlers["client/registerCapability"]
+  if handler then
+    vim.lsp.handlers["client/registerCapability"] = function(err, params, ctx, config)
+      params = params or {}
+      if params.registrations == nil then
+        params.registrations = {}
+      end
+      return handler(err, params, ctx, config)
+    end
+  end
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
