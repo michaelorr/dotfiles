f095bb28ed912e1dc3c31e59e95006d191f14ec4
Michael Orr <michael@orr.co>
2026-09-05 18:23:33 -0400

    re-enable copilot

---
+++ config.nvim/lua/plugins/copilot.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    config = function()
+      vim.lsp.config("copilot", {})
+
+      require("copilot").setup({
+        server_opts_overrides = {
+          offset_encoding = "utf-16", -- See `:h vim.lsp.start` for more info
+        },
+        copilot_model = "gpt-41-copilot",
+        suggestion = {
+          enabled = not vim.g.ai_cmp,
+          auto_trigger = true,
+          keymap = {
+            next = "<S-]>",
+            prev = "<S-[>",
+            accept = "<Tab>",
+            accept_word = "<S-Tab>",
+          },
+        },
+        panel = { auto_refresh = true },
+        filetypes = {
+          markdown = true,
+          help = true,
+        },
+      })
+    end,
+  },
+}
